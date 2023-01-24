3af1ef765b91b2ac38aa83b64d96f091c640749c
Abraham Nieva <a_nieva@hotmail.com>
2023-01-24 17:55:42 +0000

    parse json data and create objects 01

--- quick_test.rb
+++ quick_test.rb
@@ -1,13 +1,27 @@
-require 'serrano'
-s_api = Serrano
+require './lib/xref_client'
 
-s_api.configuration do |config|
-  config.mailto = "a_nieva@hotmail.com"
+def inspect_element(element_name,element_value,element_class)
+  if element_class.to_s == "Hash" 
+    puts ("* create object: "+ element_name ) 
+  elsif element_class.to_s == "Array"
+    puts ("* create list of: "+ element_name ) 
+  end
 end
 
-#puts s_api.works(ids: '10.1371/journal.pone.0033693')
 
-doi = '10.1038/s41929-019-0334-3' #first article in CDI DB
-art_bib = JSON.parse(s_api.content_negotiation(ids: doi, format: "citeproc-json"))
+doi = '10.1038/s41929-019-0334-3'
+art_bib = XrefClient.getCRData(doi)
+
 puts '*********************************************************************'
-puts art_bib
+#puts art_bib
+
+File.write('./sample-pub.json', JSON.pretty_generate(art_bib))
+class_keys=art_bib.keys()
+
+for a_key in class_keys
+  item_class = art_bib[a_key].class()
+  puts a_key + ": " + item_class.to_s()  + "\n"
+  inspect_element(a_key, art_bib[a_key], item_class)
+end
+
+
