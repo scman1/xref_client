f09f8ac1427105f7955cb98782d381329899c79e
Abraham Nieva <a_nieva@hotmail.com>
2023-01-26 18:24:51 +0000

    assing default values (varchar or integer for now)

--- quick_test.rb
+++ quick_test.rb
@@ -36,7 +36,6 @@ end
 # 6 other       notes on how to get data if not specified above
 
 # get value from an inner element
-
 def inspect_path(json_vals,a_path)
   ret_val = nil
   if json_vals[a_path[0]] != nil 
@@ -60,12 +59,26 @@ def get_inner_element(json_vals, json_paths)
   return ret_val
 end
 
+# evaluate a given expression
 def evaluate_exp(pub_data, eval_exp)
   # replace 'data_hash' with pub_data on the string to evaluate
   # return the evaluate response
   return eval(eval_exp.gsub('d_h', 'pub_data'))
 end
 
+def only_nums(a_string)
+  a_string.scan(/\D/).empty?
+end
+
+# assign a default value
+def assign_value(a_val,a_type)
+  if a_type == 'varchar'
+     return a_val
+  elsif a_type == 'integer'and only_nums(a_string)
+    return eval(a_val)
+  end
+  return nil
+end
 
 xref_pub_csv = CSV.new('./map_pub_xref_cdi.csv')
 
@@ -122,6 +135,7 @@ new_pub = cdi_pub_class.new()
 xref_pub_csv_map.each do |xref_mapping|
   if not ["NULL","NOT NULL"].include?(xref_mapping['default'])
     puts "Assing" + xref_mapping['default'] + " to " +  xref_mapping['cdi'] + " type " + xref_mapping['type']
+    xref_pub_data[xref_mapping['cdi']]  = assign_value(xref_mapping['default'],xref_mapping['type'])
   elsif xref_mapping['json_paths'] != nil
     puts "Get values for " + xref_mapping['cdi'] + " from " + xref_mapping['json_paths']
     xref_pub_data[xref_mapping['cdi']]  = get_inner_element(pub_data, eval(xref_mapping['json_paths']))
@@ -142,117 +156,5 @@ XrefClient::DigitalObjectFactory.assign_attributes(new_pub, xref_pub_data)
 puts new_pub.title
 puts new_pub.doi
 puts new_pub.pub_year
-
-# get authors and link them to article
-# get author affiliations and link them to article and 
-    def getPubData(db_article, doi_text)
-      if doi_text != ""
-        # need to raise an exeption if doi is incorrect or no data is returned
-        # need to check the doi is not in DB before saving (lower and uppercase versions)
-        # need to trim dois before saving
-        pub_data = XrefClient.getCRData(doi_text)
-        data_keys = pub_data.keys()
-        pub_columns = []
-        exclude_columns = get_excluded()
-        for key in data_keys do
-          key_cp = key.dup()
-          if not pub_columns.include?(key_cp) and not exclude_columns.include?(key_cp)
-            pub_columns.append(key_cp)
-          end
-        end
-        for key in pub_columns
-          key_cp = key.dup()
-          if key_cp.include?('-')
-            new_key = key_cp.gsub('-','_')
-            pub_columns.delete(key_cp)
-            pub_columns.append(new_key)
-            pub_data[new_key] = pub_data[key]
-            pub_data.delete(key_cp)
-          end
-        end
-        for frzkey in ['container-title', 'journal-issue']
-          pub_columns.delete(frzkey)
-          new_key = frzkey.gsub('-','_')
-          pub_columns.append(new_key)
-          pub_data[new_key] = pub_data[frzkey]
-          pub_data.delete(frzkey)
-        end
-        puts "###################################################################"
-        puts pub_columns
-        puts pub_data
-        puts "###################################################################"
-        puts pub_data['title']
-        db_article.title = pub_data['title']
-        db_article.publisher = pub_data['publisher']
-        db_article.issue = pub_data['issue']
-        db_article.pub_type = pub_data['type']
-        db_article.license = pub_data['license']
-        db_article.volume = pub_data['volume']
-        db_article.referenced_by_count = pub_data['is_referenced_by_count']
-        db_article.references_count = pub_data['references_count']
-        db_article.link = pub_data['link']
-        db_article.url = pub_data['URL']
-        if pub_data.keys.include?('journal_issue') \
-          and pub_data['journal_issue'] != nil then
-          if pub_data['journal_issue'].keys.include?('issue') then
-            db_article.journal_issue = pub_data['journal_issue']['issue']
-          end
-          if pub_data['journal_issue'].keys().include?('published-print') then
-            if pub_data['journal_issue']['published-print']['date-parts'][0].length == 1 then
-              #assume that if date parts has only one element, it is year
-              db_article.pub_ol_year = pub_data['journal_issue']['published-print']['date-parts'][0][0]
-            elsif pub_data['journal_issue']['published-print']['date-parts'][0].length == 2 then
-              #assume that if date parts has two elements, they are year and month
-              db_article.pub_ol_year = pub_data['journal_issue']['published-print']['date-parts'][0][0]
-              db_article.pub_ol_month = pub_data['journal_issue']['published-print']['date-parts'][0][1]
-            elsif pub_data['journal_issue']['published-print']['date-parts'][0].length == 3 then
-              # assume year, month and day if date parts has three elements
-              db_article.pub_print_year = pub_data['journal_issue']['published-print']['date-parts'][0][0]
-              db_article.pub_print_month = pub_data['journal_issue']['published-print']['date-parts'][0][1]
-              db_article.pub_print_day = pub_data['journal_issue']['published-print']['date-parts'][0][2]
-            end
-          end
-        end
-        db_article.container_title = pub_data['container_title']
-        db_article.page = pub_data['page']
-        db_article.abstract = pub_data['abstract']
-        if pub_data.keys.include?('published_online') then
-          if pub_data['published_online']['date-parts'][0].length == 1 then
-            #assume that if date parts has only one element, it is year
-            db_article.pub_ol_year = pub_data['published_online']['date-parts'][0][0]
-          elsif pub_data['published_online']['date-parts'].length == 2 then
-            #assume that if date parts has two elements, they are year and month
-            db_article.pub_ol_year = pub_data['published_online']['date-parts'][0][0]
-            db_article.pub_ol_month = pub_data['published_online']['date-parts'][0][1]
-          elsif pub_data['published_online']['date-parts'][0].length == 3 then
-            # assume year, month and day if date parts has three elements
-            db_article.pub_ol_year = pub_data['published_online']['date-parts'][0][0]
-            db_article.pub_ol_month = pub_data['published_online']['date-parts'][0][1]
-            db_article.pub_ol_day = pub_data['published_online']['date-parts'][0][2]
-          end
-        end
-        if pub_data.keys.include?('published_print') then
-          if pub_data['published_print']['date-parts'][0].length == 1 then
-            #assume that if date parts has only one element, it is year
-            db_article.pub_ol_year = pub_data['published_print']['date-parts'][0][0]
-          elsif pub_data['published_print']['date-parts'][0].length == 2 then
-            #assume that if date parts has two elements, they are year and month
-            db_article.pub_ol_year = pub_data['published_print']['date-parts'][0][0]
-            db_article.pub_ol_month = pub_data['published_print']['date-parts'][0][1]
-          elsif pub_data['published_print']['date-parts'][0].length == 3 then
-            # assume year, month and day if date parts has three elements
-            db_article.pub_print_year = pub_data['published_print']['date-parts'][0][0]
-            db_article.pub_print_month = pub_data['published_print']['date-parts'][0][1]
-            db_article.pub_print_day = pub_data['published_print']['date-parts'][0][2]
-          end
-        end
-        if db_article.pub_ol_year != nil
-          db_article.pub_year = db_article.pub_ol_year
-        else
-          db_article.pub_year = db_article.pub_print_year
-        end
-      end
-      # mark incomplete as it is missing authors, affiliation and themes
-      db_article.status = "Incomplete"
-    end
+puts new_pub
 
