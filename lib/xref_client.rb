07991a6fdbc49a545989613bbb97261779aa59e2
Abraham Nieva <a_nieva@hotmail.com>
2024-03-27 16:05:58 +0000

    initial change and mod to avoid identation error message

--- lib/xref_client.rb
+++ lib/xref_client.rb
@@ -144,29 +144,29 @@ module XrefClient
   class DigitalObjectFactory
     def self.create_class(new_class, *fields)
       c = Class.new do
-		fields.flatten.each do |field|
+        fields.flatten.each do |field|
           #replace backslashes and space in names with underscores
-		  field = field.gsub('/','_')
-		  field = field.gsub(' ','_')
-		  define_method field.intern do
-		    instance_variable_get("@#{field}")
-		  end
-		  define_method "#{field}=".intern do |arg|
-		    instance_variable_set("@#{field}", arg)
-		  end
-		end
-	  end
-	  XrefClient.const_set new_class, c
-	  return c
-	end
+          field = field.gsub('/','_')
+          field = field.gsub(' ','_')
+          define_method field.intern do
+            instance_variable_get("@#{field}")
+          end
+          define_method "#{field}=".intern do |arg|
+            instance_variable_set("@#{field}", arg)
+          end
+        end
+      end
+      XrefClient.const_set new_class, c
+      return c
+    end
 
-	def self.assign_attributes(instance, values)
-	  values.each do |field, arg|
-	    #replace backslashes and space in names with underscores
-		  field = field.gsub('/','_')
-		  field = field.gsub(' ','_')
-		  instance.instance_variable_set("@#{field}", arg)
-	  end
-	end
+    def self.assign_attributes(instance, values)
+      values.each do |field, arg|
+        #replace backslashes and space in names with underscores
+          field = field.gsub('/','_')
+          field = field.gsub(' ','_')
+          instance.instance_variable_set("@#{field}", arg)
+      end
+    end
   end
 end
